d30a279b4516d19af02ae47c7612e0a3c3b63278
dosh <18360424+dosh3226@users.noreply.github.com>
2021-08-02 23:11:37 +1000

    Add files via upload

--- light_sky.ino
+++ light_sky.ino
@@ -49,11 +49,12 @@
   */
 
 // number of LEDs total
+// #define led_string_length 432
 #define led_string_length 100
 // number of same coloured pixel groups
-#define num_groups 3
+
 // number of members in a group
-#define group_length 30
+// #define group_length 15
 
 #include <Adafruit_NeoPixel.h>
 
@@ -64,7 +65,6 @@
 Adafruit_NeoPixel pixels(led_string_length, PIN, NEO_GRB + NEO_KHZ800);
 
 typedef struct Pixels {
-	byte group;
 	// address is done with chain[i] so not needed here
 	byte red; // 0 to 255
 	byte green; // 0 to 255
@@ -74,6 +74,48 @@ typedef struct Pixels {
 } Pixel;
 
 Pixel chain[led_string_length];
+
+byte idx, max_brightness, red, green, blue = 0;
+
+void group_setup(int num_groups, int group_length) {
+	
+	for(int i = 0; i < led_string_length; i++) {
+		chain[i].red = 0;
+		chain[i].green = 0;
+		chain[i].blue = 0;
+		chain[i].curr_brightness = 0;
+		chain[i].max_brightness = 0;
+	}
+	
+	for(int group = 0; group < num_groups; group++) {
+		// use brightnesses between 5 and 40%
+		// 40% max is good from testing with adafruit_neopixel.ino
+		max_brightness = random(5, 40);
+		red = random(0, 255);
+		green = random(0, 255);
+		blue = random(0, 255);
+		for(int member = 0; member < group_length; member++) {
+			idx = random(0, led_string_length);
+			// check if idx is already populated
+			if(chain[idx].max_brightness == 0) {
+				chain[idx].red = red;
+				chain[idx].green = green;
+				chain[idx].blue = blue;
+				chain[idx].curr_brightness = (100 * group) / (num_groups);
+				chain[idx].max_brightness = max_brightness;
+			} else {
+				member--;
+			}
+		}
+	}
+	
+	// // now stagger the brightnesses of all the groups
+	// for(int i = 0; i < led_string_length; i++) {
+		// if(chain[i].max_brightness != 0) {
+			// chain[i].curr_brightness = (100 * (chain[i].group)) / (num_groups);
+		// }
+	// }
+}
 
 void setup() {
     Serial.begin(115200);
@@ -90,7 +132,6 @@ void setup() {
 	
 	// default chain values
 	for(int i = 0; i < led_string_length; i++) {
-		chain[i].group = 255;
 		chain[i].red = 0;
 		chain[i].green = 0;
 		chain[i].blue = 0;
@@ -99,36 +140,7 @@ void setup() {
 	}
 
 	// first time group setup
-	byte idx, max_brightness, red, green, blue = 0;
-	for(int group = 0; group < num_groups; group++) {
-		// use brightnesses between 5 and 40%
-		// 40% max is good from testing with adafruit_neopixel.ino
-		max_brightness = random(5, 40);
-		red = random(0, 255);
-		green = random(0, 255);
-		blue = random(0, 255);
-		for(int member = 0; member < group_length; member++) {
-			idx = random(0, led_string_length);
-			// check if idx is already populated
-			if(chain[idx].max_brightness == 0) {
-				chain[idx].group = group;
-				chain[idx].red = red;
-				chain[idx].green = green;
-				chain[idx].blue = blue;
-				chain[idx].curr_brightness = (100 * (chain[idx].group)) / (num_groups);
-				chain[idx].max_brightness = max_brightness;
-			} else {
-				member--;
-			}
-		}
-	}
-	
-	// now stagger the brightnesses of all the groups
-	for(int i = 0; i < led_string_length; i++) {
-		if(chain[i].group != 255) {
-			chain[i].curr_brightness = (100 * (chain[i].group)) / (num_groups);
-		}
-	}
+	group_setup(10, 10);
 	
 	Serial.println(F("debug print loop start"));
 	// check the setup by printing the array vals to serial
@@ -137,8 +149,6 @@ void setup() {
 			Serial.print(F("("));
 			Serial.print(i+j);
 			Serial.print(F("),"));
-			Serial.print(chain[i + j].group, DEC);
-			Serial.print(F(","));
 			Serial.print(chain[i + j].curr_brightness, DEC);
 			Serial.print(F(","));
 			Serial.print(chain[i + j].red, DEC);
@@ -149,33 +159,114 @@ void setup() {
 	Serial.println(F("setup loop complete"));
 }
 
-void loop() {
+void single_flash() {
+	
+	int num_groups = 3;
+	int group_length = 10;
+	
     // turn off all pixels
     pixels.clear();
-	// reset the chain values
-	// for(int i = 0; i < led_string_length; i++) {
-		// chain[i].red = 0;
-		// chain[i].green = 0;
-		// chain[i].blue = 0;
-		// chain[i].curr_brightness = 0;
-		// chain[i].max_brightness = 0;
-	// }
 	
+	Serial.println(F("group_setup"));
 	
-	// // new groups setup
-	// int idx, max_brightness = 0;
-	// for(int group = 0; group < num_groups; group++) {
-		// // use brightnesses between 5 and 40%
-		// // 40% max is good from testing with adafruit_neopixel.ino
-		// max_brightness = random(5, 40);
-		// for(int member = 0; member < group_length; member++) {
-			// idx = random(0, led_string_length);
-			// // check if idx is already populated
-			// if(chain[idx].max_brightness != 0) {
-				// continue;
-			// }
-		// }
-	// }
+	group_setup(num_groups, group_length);
+	
+	Serial.println(F("out of group_setup"));
+	
+	int b = 0;
+	int i = 0;
+	for (b = 0; b < 40; b++) {
+		for(i = 0; i < led_string_length; i++) {
+			pixels.setPixelColor(i, pixels.Color(
+				(chain[i].red * b)/100, 
+				(chain[i].green * b)/100, 
+				(chain[i].blue * b)/100));
+		}
+		pixels.show();
+		delay(5);
+	}
+	
+	for (b = 40; b > 0; b--) {
+		for(i = 0; i < led_string_length; i++) {
+			pixels.setPixelColor(i, pixels.Color(
+				(chain[i].red * b)/100, 
+				(chain[i].green * b)/100, 
+				(chain[i].blue * b)/100));
+		}
+		pixels.show();
+		delay(5);
+	}
+}
+
+void weird_effect_fast() {
+		
+	group_setup(10, 10);
+		
+	byte new_r = random(0, 255);
+	byte new_g = random(0, 255);
+	byte new_b = random(0, 255);
+	byte max_brightness = random(1, 10);
+	// check if any groups have flashed once and get new rgb and max_b vals for those pixels
+	for(int i = 0; i < led_string_length; i++) {
+		if(chain[i].curr_brightness >= 50) {
+			chain[i].red = new_r;
+			chain[i].green = new_g;
+			chain[i].blue = new_b;
+			chain[i].max_brightness = max_brightness;
+			chain[i].curr_brightness = 0;
+		} else {
+			chain[i].curr_brightness++;
+		}
+	}
+	
+	for(int i = 0; i < led_string_length; i++) {
+		pixels.setPixelColor(i, 
+			pixels.Color(
+				(chain[i].red * chain[i].curr_brightness)/200, 
+				(chain[i].green * chain[i].curr_brightness)/200, 
+				(chain[i].blue * chain[i].curr_brightness)/200
+			)
+		);
+	}
+	
+	pixels.show();
+	delay(5);
+}
+
+void weird_effect() {
+	byte new_r = random(0, 255);
+	byte new_g = random(0, 255);
+	byte new_b = random(0, 255);
+	byte max_brightness = random(1, 10);
+	// check if any groups have flashed once and get new rgb and max_b vals for those pixels
+	for(int i = 0; i < led_string_length; i++) {
+		if(chain[i].curr_brightness >= 30) {
+			chain[i].red = new_r;
+			chain[i].green = new_g;
+			chain[i].blue = new_b;
+			chain[i].max_brightness = max_brightness;
+			chain[i].curr_brightness = 0;
+		} else {
+			chain[i].curr_brightness++;
+		}
+	}
+	
+	for(int i = 0; i < led_string_length; i++) {
+		pixels.setPixelColor(i, 
+			pixels.Color(
+				(chain[i].red * chain[i].curr_brightness)/200, 
+				(chain[i].green * chain[i].curr_brightness)/200, 
+				(chain[i].blue * chain[i].curr_brightness)/200
+			)
+		);
+	}
+	
+	pixels.show();
+	delay(20);
+}
+
+void loop() {
+	weird_effect();
 }
 
 
